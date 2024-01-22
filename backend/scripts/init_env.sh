288cb3d5b4a878760acfe26e0f1b3913bc1ef129
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2024-01-22 18:32:16 +0300

    feat: Add multi environment dotenv managment lib
    * refactor: Update init_env script to match dotenv-flow lib
    
    * chore: Add test and development envs to gitignore

--- backend/scripts/init_env.sh
+++ backend/scripts/init_env.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-if [ -e .env ]; then
-    printf '.env already initialized, remove it and try again' >&2
-    exit 1
-else
-    if [ -e .env.template ]; then
-        printf 'initializing .env file...'
-        cp .env.template .env
+if [ -e .env.template ]; then
+    printf 'initializing .env.* files...\n'
+    if [ -e .env.development ]; then
+        printf 'WARNING: .env.development already exists, remove it to initialize\n'
     else
-        printf '.env.template not exists' >&2
-        exit 1
+        cp .env.template .env.development
     fi
+    if [ -e .env.test ]; then
+        printf 'WARNING: .env.test already exists, remove it to initialize\n'
+    else
+        cp .env.template .env.test
+    fi
+else
+    printf '.env.template not exists' >&2
+    exit 1
 fi

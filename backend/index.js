80a4d176c532eb77644abb2c54e7714e2f9c4f57
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2024-01-22 11:46:28 +0300

    feat: Add prometheus client to backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import process from 'process';
+import promBundle from 'express-prom-bundle';
+
 const app = express();
 const port = 3000;
+const promMiddleware = promBundle({
+    includeUp: true,
+    includeMethod: true,
+    includePath: true,
+    includeStatusCode: true,
+});
+
+app.use(promMiddleware);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({
+        'GET /metrics': 'get metrics data',
+    });
 });
 
 app.listen(port, () => {
